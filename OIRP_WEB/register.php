6a993cac4e7b0c8cd66717c86dba414972092419
EunminM <dongbaekminsoo@gmail.com>
2018-02-10 19:31:39 +0800

    register.php

--- OIRP_WEB/register.php
+++ OIRP_WEB/register.php
@@ -12,13 +12,16 @@
         <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">
 	</head>
 	<body>
-		<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-        <script src="bootstrap-3.3.7-dist/js/jquery-3.0.0.min.js"></script>
-        
-		<input type="text" name="student_id" placeholder="Enter Email"/>
-		<br>
-		<input type="password" name="password" placeholder="Enter Email"/>
-		<br>
-		<input type="password" name="ret_try_pass" placeholder="Enter Email"/>
-		<br>
-		
+		<div>
+			<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+	        <script src="bootstrap-3.3.7-dist/js/jquery-3.0.0.min.js"></script>
+	        
+			<input type="text" name="student_id" placeholder="Enter Email"/>
+			<br>
+			<input type="password" name="password" placeholder="Enter Email"/>
+			<br>
+			<input type="password" name="ret_try_pass" placeholder="Enter Email"/>
+			<br>
+		</div>
+	</body>
+</html>	
