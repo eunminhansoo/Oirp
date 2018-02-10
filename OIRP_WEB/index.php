ad9545d0561bd1d7723b5b6cab10608f7e3e7913
EunminM <dongbaekminsoo@gmail.com>
2018-02-10 16:51:42 +0800

    input email and input password

--- OIRP_WEB/index.php
+++ OIRP_WEB/index.php
@@ -6,16 +6,23 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width = device-width, initial-scale = 1">
-        <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap-theme.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">
 	</head>
 	<body>
-		<script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery-3.0.0.min.js"></script>
-		<div>
-			<input type="text" name="">
+		<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+        <script src="bootstrap-3.3.7-dist/js/jquery-3.0.0.min.js"></script>
+		<div class="container">
+			<form action="#">
+				<div class="form-group">
+					<input type="text" name="userName" class="input form-control" placeholder="Enter Email"/>
+				</div>
+				<div class="form-group">
+					<input type="text" name="userName" class="input form-control" placeholder="Enter Password">
+				</div>
+			</form>
 		</div>
 	</body>
 </html>
