8c6efc5a2c3088f8eb99da1874f21ac64cfe28d6
davelpo <davelportega@gmail.com>
2018-02-10 19:18:51 +0800

    look

---
+++ OIRP_WEB/register.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width = device-width, initial-scale = 1">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap-theme.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+	</head>
+	<body>
+		<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+        <script src="bootstrap-3.3.7-dist/js/jquery-3.0.0.min.js"></script>
+        
+		<input type="text" name="student_id" placeholder="Enter Email"/>
+		<br>
+		<input type="password" name="password" placeholder="Enter Email"/>
+		<br>
+		<input type="password" name="ret_try_pass" placeholder="Enter Email"/>
+		<br>
+		
