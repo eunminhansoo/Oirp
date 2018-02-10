b14791cc46eaf3226667ec7396da8c428f0694de
EunminM <dongbaekminsoo@gmail.com>
2018-02-10 19:29:55 +0800

    additional in css: style.css and index.php

--- OIRP_WEB/index.php
+++ OIRP_WEB/index.php
@@ -10,18 +10,23 @@
         <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap-theme.css">
         <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/style.css">
 	</head>
 	<body>
 		<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
         <script src="bootstrap-3.3.7-dist/js/jquery-3.0.0.min.js"></script>
-		<div class="container">
+		<div class="container div_center">
 			<form action="#">
 				<div class="form-group">
 					<input type="text" name="userName" class="input form-control" placeholder="Enter Email"/>
 				</div>
 				<div class="form-group">
-					<input type="text" name="userName" class="input form-control" placeholder="Enter Password">
+					<input type="text" name="userName" class="input form-control" placeholder="Enter Password"/>
 				</div>
+				<div>
+					<p>New user? <a href="#">Create new account</a></p>
+				</div>
+				<button type="submit" class="btn button_style">Submit</button>
 			</form>
 		</div>
 	</body>
