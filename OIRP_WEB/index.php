18e2ca7ec9867a3febff76e0a1fcdc4c0f1a3293
EunminM <dongbaekminsoo@gmail.com>
2018-02-10 15:18:33 +0800

    added bootstrap folder and input the tags

--- OIRP_WEB/index.php
+++ OIRP_WEB/index.php
@@ -1,12 +1,19 @@
 <?php
 	
 ?>
-
+<!DOCTYPE html>
 <html>
 	<head>
-	
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width = device-width, initial-scale = 1">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	</head>
 	<body>
+		<script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery-3.0.0.min.js"></script>
 		<div>
 			<input type="text" name="">
 		</div>
