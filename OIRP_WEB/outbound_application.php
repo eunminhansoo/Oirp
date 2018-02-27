1d7b726faf84e0c6d43ef24993e8e6d1c993be6c
davelp <davelportega@gmail.com>
2018-02-28 00:08:40 +0800

    commit

--- OIRP_WEB/outbound_application.php
+++ OIRP_WEB/outbound_application.php
@@ -1,3 +1,18 @@
 <?php
+	include 'database_connection.php';
+    
+	$message = '';
+	
+	if(isset($_POST['btn_outform1'])){
+	$citizenship_out = $_POST['citizenship_out'];
+	$nationality_out = $_POST['nationality_out'];
+	$passport_num_out = $_POST['passport_num_out'];
+	$date_issuance_out = $_POST['date_issuance_out'];
+	$validity_date_out = $_POST['validity_date_out'];
+	$mailing_add_out = $_POST['mailing_add_out'];
+	$telephone_num_out = $_POST['telephone_num_out'];
+	$mobile_num_out = $_POST['mobile_num_out'];	
+	
+	}
 
 ?>
