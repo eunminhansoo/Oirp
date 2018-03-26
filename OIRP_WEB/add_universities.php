83005a69c3909017e011eb22f82948c6b9178691
Jeanina <2014072568@ust-ics.mygbiz.com>
2018-03-26 15:32:53 +0800

    tinanggal na yung DataManagementSystem na folder yung add universities para sa admin, paayos na lang din

---
+++ OIRP_WEB/add_universities.php
@@ -0,0 +1,32 @@
+<?php
+
+	require 'database_connection.php';
+	
+	error_reporting(0);
+	
+	$country = $_POST['country'];
+	$university = $_POST['university'];
+	
+	$sql = "select university,country from partner_universities where university = '".$university."'";
+	$result = mysqli_query($conn, $sql);
+	
+	while ($row = $result->fetch_array()){
+		$univ = $row['university'];
+		$coun = $row['country'];
+		
+		if ($univ==$university && $coun==$country){
+			echo $university." is already recorded.";
+		} else{
+			$sql = "insert into partner_universities(university,country) values ('".$university."', '".$country."')";
+			$result = mysqli_query($conn, $sql);
+			
+			echo "1 row inserted.";
+		}
+		
+	}
+	
+
+	
+	
+?>
+
