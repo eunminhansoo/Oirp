ab155dcfd3b6a0659de469cd916ce4cffa67096f
Jeanina <2014072568@ust-ics.mygbiz.com>
2018-03-31 00:21:33 +0800

    UI etc

--- OIRP_WEB/outboundform2.php
+++ OIRP_WEB/outboundform2.php
@@ -34,34 +34,34 @@
 					<div class="form-group row">
 						<div class="col-sm-10">
 							<label>Father's Name</label>
-							<input type="text" name="father" id="father" class="form-control" pattern="[^0-9<>].{1,20}" maxlength="60" value="<?php echo $getSel_FATHER_NAME_OUT?>" required>
+							<input type="text" name="father" id="father" class="form-control" pattern="[^0-9<>].{1,60}" maxlength="60" value="<?php echo $getSel_FATHER_NAME_OUT?>" required>
 						</div>
 					</div>
 					<div class="form-group row">
 						<div class="col-sm-5">
 							<label>Occupation</label>
-							<input type="text" name="fOccupation" id="fOccupation" class="form-control" pattern="[^<>].{1,20}" maxlength="20" value="<?php echo $getSel_OCCUPATION_DADA_OUT?>" required>
+							<input type="text" name="fOccupation" id="fOccupation" class="form-control" pattern="[^<>].{1,20}" maxlength="20" value="<?php echo $getSel_OCCUPATION_DADA_OUT?>">
 						</div>
 						<div class="col-sm-5">
 							<label>Company</label>
-							<input type="text" name="fCompany" id="fCompany" class="form-control" pattern="[^<>].{1,20}" maxlength="20" value="<?php echo $getSel_COMPANY_DADA_OUT?>" required>
+							<input type="text" name="fCompany" id="fCompany" class="form-control" pattern="[^<>].{1,20}" maxlength="20" value="<?php echo $getSel_COMPANY_DADA_OUT?>">
 						</div>
 					</div>
 					<div class="form-group row">
 						<div class="col-sm-10">
 							<label>Address</label>
-							<input type="text" name="fAddress" id="fAddress" class="form-control" pattern="[^<>].{1,115}" maxlength="115" value="<?php echo $getSel_ADDRESS_DADA_OUT?>" required>
+							<input type="text" name="fAddress" id="fAddress" class="form-control" pattern="[^<>].{1,115}" maxlength="115" value="<?php echo $getSel_ADDRESS_DADA_OUT?>">
 						</div>
 					</div>
 					<div class="form-group row">
 						<div class="col-sm-4">
 							<label>Contact Number</label>
 							<p class="text_style" >Phone number or Mobile number</p>
-							<input type="text" name="fNumber" id="fNumber" class="form-control" placeholder="ex: +63 2-406-1611 or +63 974-887-6512" pattern="([+]\d{1,4})([\s]\d{1,3}[-]\d{2,3}[-]\d{3,4})"  maxlength=30 value="<?php echo $getSel_CONTACT_NUM_DADA_OUT?>" required>
+							<input type="text" name="fNumber" id="fNumber" class="form-control" placeholder="ex: +63 2-406-1611 or +63 974-887-6512" pattern="([+]\d{1,4})([\s]\d{1,3}[-]\d{2,3}[-]\d{3,4})"  maxlength=30 value="<?php echo $getSel_CONTACT_NUM_DADA_OUT?>">
 						</div>
 						<div class="col-sm-6">
 							<label>Email Address</label>
-							<input type="email" name="fEmail" id="fEmail" class="form-control" value="<?php echo $getSel_EMAIL_ADD_DADA_OUT?>" required>
+							<input type="email" name="fEmail" id="fEmail" class="form-control" value="<?php echo $getSel_EMAIL_ADD_DADA_OUT?>">
 						</div>
 					</div>
 					<div class="form-group row">
@@ -81,34 +81,34 @@
 					<div class="form-group row break">
 						<div class="col-sm-10">
 							<label>Mother's Name</label>
-							<input type="text" name="mother" id="mother" class="form-control" pattern="[^0-9<>].{1,20}" maxlength="60" value="<?php echo $getSel_MOTHER_NAME_OUT?>" required>
+							<input type="text" name="mother" id="mother" class="form-control" pattern="[^0-9<>].{1,60}" maxlength="60" value="<?php echo $getSel_MOTHER_NAME_OUT?>" required>
 						</div>
 					</div>
 					<div class="form-group row">
 						<div class="col-sm-5">
 							<label>Occupation</label>
-							<input type="text" name="mOccupation" id="mOccupation" class="form-control" pattern="[^<>].{1,20}" maxlength="20" value="<?php echo $getSel_OCCUPATION_MOM_OUT?>" required>
+							<input type="text" name="mOccupation" id="mOccupation" class="form-control" pattern="[^<>].{1,20}" maxlength="20" value="<?php echo $getSel_OCCUPATION_MOM_OUT?>">
 						</div>
 						<div class="col-sm-5">
 							<label>Company</label>
-							<input type="text" name="mCompany" id="mCompany" class="form-control" pattern="[^<>].{1,20}" maxlength="20" value="<?php echo $getSel_COMPANY_MOM_OUT?>" required>
+							<input type="text" name="mCompany" id="mCompany" class="form-control" pattern="[^<>].{1,20}" maxlength="20" value="<?php echo $getSel_COMPANY_MOM_OUT?>">
 						</div>
 					</div>
 					<div class="form-group row">
 						<div class="col-sm-10">
 							<label>Address</label>
-							<input type="text" name="mAddress" id="mAddress" class="form-control" pattern="[^<>].{1,115}" maxlength="115" value="<?php echo $getSel_ADDRESS_MOM_OUT?>" required> 
+							<input type="text" name="mAddress" id="mAddress" class="form-control" pattern="[^<>].{1,115}" maxlength="115" value="<?php echo $getSel_ADDRESS_MOM_OUT?>"> 
 						</div>
 					</div>
 					<div class="form-group row">
 						<div class="col-sm-4">
 							<label>Contact Number</label>
 							<p class="text_style" >Phone number or Mobile number</p>
-							<input type="text" name="mNumber" id="mNumber" class="form-control" placeholder="ex: +63 2-406-1611 or +63 974-887-6512" pattern="([+]\d{1,4})([\s]\d{1,3}[-]\d{2,3}[-]\d{3,4})" maxlength=30 value="<?php echo $getSel_CONTACT_NUM_MOM_OUT?>" required>
+							<input type="text" name="mNumber" id="mNumber" class="form-control" placeholder="ex: +63 2-406-1611 or +63 974-887-6512" pattern="([+]\d{1,4})([\s]\d{1,3}[-]\d{2,3}[-]\d{3,4})" maxlength=30 value="<?php echo $getSel_CONTACT_NUM_MOM_OUT?>">
 						</div>
 						<div class="col-sm-6">
 							<label>Email Address</label>
-							<input type="email" name="mEmail" id="mEmail" class="form-control" value="<?php echo $getSel_EMAIL_ADD_MOM_OUT?>" required>
+							<input type="email" name="mEmail" id="mEmail" class="form-control" value="<?php echo $getSel_EMAIL_ADD_MOM_OUT?>">
 						</div>
 					</div>
 					<div class="form-group row">
@@ -128,7 +128,7 @@
 					
 					<div class="form-group row break col-xs-5">
 						<div class="col-sm-10">
-							<button type="submit" class="btn btn-primary" formaction="inboundform1.php">Previous</button>
+							<button type="submit" class="btn btn-primary" formaction="outboundform1.php">Previous</button>
 						</div>
 					</div>
 					<div class="form-group row break col-xs-5">
