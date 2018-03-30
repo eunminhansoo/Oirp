671f24c4b6a715b6fbbd724bec2fa738b4d425b7
Jeanina <2014072568@ust-ics.mygbiz.com>
2018-03-31 00:25:43 +0800

    UI etc

--- OIRP_WEB/student_home.php
+++ OIRP_WEB/student_home.php
@@ -13,8 +13,8 @@
         <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/custom.css">
         <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap-theme.css">
         <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">
-		<!--<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/stylus.css">-->
-		<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/color_3.css">
+		<!-- link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/stylus.css">
+		<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/color_3.css"-->
 		<link rel="icon" href="img/ust.png" type="image/png" sizes="196x196">
 	</head>
 	
