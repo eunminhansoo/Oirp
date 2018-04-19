af0e9f69bb61e847cecac3808607906adf21cefb
EunminM <dongbaekminsoo@gmail.com>
2018-04-19 13:08:20 +0800

    rename

--- OIRP_WEB/graph_button.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    $get_year = $_SESSION['$set_yearly'];
-    if($get_year == NULL){
-        $get_year = '2015';
-        session_destroy();
-    }
-    echo $get_year;
-?>
