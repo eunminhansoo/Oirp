9706038758e435065ba13d27d8f9b7e25a1d5be5
EunminM <dongbaekminsoo@gmail.com>
2018-04-22 23:48:38 +0800

    TOR_SCAN table

--- db_v1/upload_pdf.sql
+++ db_v1/upload_pdf.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2018 at 04:51 PM
+-- Generation Time: Apr 22, 2018 at 05:45 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -37,6 +37,17 @@ CREATE TABLE `upload_pdf` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `upload_pdf`
+--
+
+INSERT INTO `upload_pdf` (`STUDENT_COUNT`, `STUDENT_ID`, `APPLICATION_PROG`, `PDF_NAME`, `PDF_IMG`, `TOR_SCAN`, `DATE_SUBMITTED`) VALUES
+(1, '20180422001-in', 'inbound', ' Lee', 'leejieun.pdf', '2.JPG', '2018-04-22'),
+(2, '20180422002-out', 'outbound', 'Orterga', 'ortega.pdf', '5.JPG', '2018-04-22'),
+(3, '20180422002-out', 'outbound', 'Orterga', 'ortega.pdf', 'PAGE1.jpg', '2018-04-22'),
+(4, '20180422001-in', 'inbound', ' Lee', 'kimjisoo.pdf', 'cert.jpg', '2018-04-22'),
+(5, '20180422003-out', 'outbound', 'Milabo', 'milabo.pdf', 'PUTA ACKER.png', '2018-04-22');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -54,7 +65,7 @@ ALTER TABLE `upload_pdf`
 -- AUTO_INCREMENT for table `upload_pdf`
 --
 ALTER TABLE `upload_pdf`
-  MODIFY `STUDENT_COUNT` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `STUDENT_COUNT` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
