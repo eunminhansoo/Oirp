38da7d2f344392b1864086f5f25f227985f85864
EunminM <dongbaekminsoo@gmail.com>
2018-04-23 04:44:49 +0800

    HELLO UPDATE

--- OIRP_WEB/approved_students.php
+++ OIRP_WEB/approved_students.php
@@ -84,13 +84,12 @@
 						</nav>
 		<!--NAV BART END-->
 		<form>
-			<div class="container-fluid">
-				<h2>Approved Students</h2>
-				<div class="col-sm-7">
+			<div class="container">
+				<h1>Qualified Students</h1>
+				<div class="">
 	        		<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search" class="form-control">
 	            </div>
-				
-				 <div class="col-sm-6">
+				<div class="">
 	                <h2>INBOUND</h2>
 	                <div class="table-responsive">
 	                    <table class="table table-striped table-bordered table-hover" id="tbl_student_in" >
@@ -99,102 +98,64 @@
 	                                <th>NAME</th>
 	                                <th>TYPE OF PROGRAM</th>
 	                                <th>DURATION / SCHOLARSHIP</th>
+									<th>COURSE</th>
 	                                <th>DATE SUBMITTED</th>
 	                                <th>STATUS</th>
-	                                <th><button type="submit" name="delete_inbound" class="btn btn-secondary" ><span class="glyphicon glyphicon-trash"></span></button></th>
+	                                <th><button type="submit" name="delete_inbound" class="btn btn-primary" ><span class="glyphicon glyphicon-trash"></span></button></th>
 	                            </tr>
 	                        </thead>
 	                        <tbody>
-	                            <?php while($row1 = mysqli_fetch_array($query1)){ 
-	                                $studentID = $row1['STUDENT_ID'];
-	                                $fullname = $row1['FAMILY_NAME'].", ".$row1['GIVEN_NAME']." ".$row1['MIDDLE_NAME'];
-	                                $ddate = $row1['DATE_ENROLL'];
-	                                $date = new DateTime($ddate);
-									$status = $row1['STATUS'];
-									$get_TYPE_OF_PROGRAM = $row1['TYPE_OF_PROGRAM'];
-									$get_TYPE_OF_FORM = $row1['TYPE_OF_FORM'];
-			                        $resultdate = $date->format('F j, Y,');
-	                            ?>
 	                            <tfoot>
+								<?php
+									$col_query = "SELECT * FROM admin_college WHERE COLLEGE = '$college' ";
+									$col_db = mysqli_query($conn, $col_query);
+									while($col_row = mysqli_fetch_array($col_db)){
+										$studentid = $col_row['STUDENT_ID'];
+										$getCollege = $col_row['COLLEGE'];
+									}
+										$sql_query = "SELECT * FROM student a INNER JOIN admin_college b ON a.STUDENT_ID = b.STUDENT_ID INNER JOIN educ_background_inbound c 
+										ON a.STUDENT_ID = c.STUDENT_ID INNER JOIN upload_pdf d ON d.STUDENT_ID = a.STUDENT_ID WHERE a.STATUS = '$college'";
+										$sql_db = mysqli_query($conn, $sql_query);
+										while($sqlRow = mysqli_fetch_array($sql_db)){
+											$fullname = $sqlRow['FAMILY_NAME'].", ".$sqlRow['GIVEN_NAME']." ".$sqlRow['MIDDLE_NAME'];
+											$get_TYPE_OF_PROGRAM = $sqlRow['TYPE_OF_PROGRAM'];
+											$get_TYPE_OF_FORM = $sqlRow['TYPE_OF_FORM'];
+											$get_TYPE_OF_FORM_OTHER = $sqlRow['TYPE_OF_FORM_OTHER'];
+											$resultdate = $sqlRow['DATE_SUBMITTED'];
+											$status = $sqlRow['STATUS'];
+											$course = $sqlRow['COURSE'];
+								?>
 	                            <tr>
-	                                <td><?php echo "<a href=admin_student_application_in.php?studentName=".urlencode($studentID).">".$fullname."</a>" ?></td>
-									<?php
-										if($get_TYPE_OF_PROGRAM == 'Others'){
-											echo "<td>Bilateral</td>";
-											echo "<td>".$row1['TYPE_OF_PROG_OTHER']."</td>";
-										}else{
+	                                <td><?php echo "<a href=admin_college_form.php?studentName=".urlencode($studentid).">".$fullname."</a>";?></td>
+									<?php 
 											echo "<td>".$get_TYPE_OF_PROGRAM."</td>";
-											if($get_TYPE_OF_FORM == 'OTHERS'){
-												echo "<td>".$row1['TYPE_OF_FORM_OTHER']."</td>";
-											}else{
-												echo "<td>".$get_TYPE_OF_FORM."</td>";
+
+											if($get_TYPE_OF_PROGRAM == "Scholarship"){
+												if($get_TYPE_OF_FORM  == "OTHERS"){
+													echo "<td>".$sqlRow['TYPE_OF_FORM_OTHER']."</td>";
+												}else{
+													echo "<td>".$get_TYPE_OF_FORM."</td>";
+												}
+											}else if($get_TYPE_OF_PROGRAM == "ShortStudy" || $get_TYPE_OF_PROGRAM == "StudyTour" || $get_TYPE_OF_PROGRAM == "ServiceLearning"){
+												if($get_TYPE_OF_FORM == "Others"){
+													echo "<td>".$get_TYPE_OF_FORM_OTHER."</td>";
+												}else{
+													echo "<td>".$get_TYPE_OF_FORM."</td>";
+												}
 											}
-										}
 									?>
-	                                <!--<td><?php //echo $row['TYPE_OF_PROGRAM']; ?></td>
-	                                <td><?php //echo $row['APPLICATION_TYPE_PROG'].": ".$row['APPLICATION_FORM']; ?></td>-->
-									<td><?php echo $resultdate ?></td>
+									<td><?php echo $course;?></td>
+									<td><?php echo $resultdate; ?></td>
 	                                <td><?php echo $status?></td>
-	                                <td><input type="checkbox" name="cb_num_in[]" value="<?php echo $studentID ?>"></td>
+	                                <td><input type="checkbox" name="cb_num_in[]" value="<?php echo $studentid?>"></td>
+										
+									<?php }?>		
 	                            </tr>
-	                        <?php } ?>
+	                        
 	                        </tfoot> 
 	                        </tbody>
 	                    </table>
 	                </div>
-	               </div>
-	                <div class="col-sm-6">
-	                <h2>OUTBOUND</h2>
-	                <div class="table-responsive">
-	                    <table class="table table-striped table-bordered table-hover" id="tbl_student_out" >
-	                        <thead>
-	                            <tr>
-	                                <th>NAME</th>
-	                                <th>TYPE OF PROGRAM</th>
-	                                <th>DURATION / SCHOLARSHIP</th>
-	                                <th>DATE SUBMITTED</th>
-	                                <th>STATUS</th>
-	                                <th><button type="submit" name="delete_inbound" class="btn btn-secondary" ><span class="glyphicon glyphicon-trash"></span></button></th>
-	                            </tr>
-	                        </thead>
-	                        <tbody>
-	                            <?php while($row = mysqli_fetch_array($query)){ 
-	                                $studentID = $row['STUDENT_ID'];
-	                                $fullname = $row['FAMILY_NAME'].", ".$row['GIVEN_NAME']." ".$row['MIDDLE_NAME'];
-	                                $ddate = $row['DATE_ENROLL'];
-	                                $date = new DateTime($ddate);
-									$status = $row['STATUS'];
-									$get_TYPE_OF_PROGRAM = $row['TYPE_OF_PROGRAM'];
-									$get_TYPE_OF_FORM = $row['TYPE_OF_FORM'];
-			                        $resultdate = $date->format('F j, Y');
-	                            ?>
-	                            <tfoot>
-	                            <tr>
-	                                <td><?php echo "$fullname" ?></td>
-									<?php
-										if($get_TYPE_OF_PROGRAM == 'Others'){
-											echo "<td>Bilateral</td>";
-											echo "<td>".$row['TYPE_OF_PROG_OTHER']."</td>";
-										}else{
-											echo "<td>".$get_TYPE_OF_PROGRAM."</td>";
-											if($get_TYPE_OF_FORM == 'OTHERS'){
-												echo "<td>".$row['TYPE_OF_FORM_OTHER']."</td>";
-											}else{
-												echo "<td>".$get_TYPE_OF_FORM."</td>";
-											}
-										}
-									?>
-	                                <!--<td><?php //echo $row['TYPE_OF_PROGRAM']; ?></td>
-	                                <td><?php //echo $row['APPLICATION_TYPE_PROG'].": ".$row['APPLICATION_FORM']; ?></td>-->
-									<td><?php echo $resultdate ?></td>
-	                                <td><?php echo $status?></td>
-	                                <td><input type="checkbox" name="cb_num_in[]" value="<?php echo $studentID ?>"></td>
-	                            </tr>
-	                        <?php } ?>
-	                        </tfoot> 
-	                        </tbody>
-	                    </table>
-	            </div>
 	            </div>
 			</div>
 		</form>
