b95f266538e89e0b71601a05d0827268a1f8e754
davelp <davelportega@gmail.com>
2018-04-23 16:59:48 +0800

    DB

--- db_v1/notification.sql
+++ db_v1/notification.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2018 at 01:34 PM
+-- Generation Time: Apr 23, 2018 at 10:58 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -36,6 +36,7 @@ CREATE TABLE `notification` (
   `COMMENT_STATUS` int(11) NOT NULL,
   `APPLICATION_FORM` varchar(500) NOT NULL,
   `COLLEGE` varchar(50) NOT NULL,
+  `COURSE` varchar(100) NOT NULL,
   `STATUS` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -43,13 +44,15 @@ CREATE TABLE `notification` (
 -- Dumping data for table `notification`
 --
 
-INSERT INTO `notification` (`STUDENT_COUNT`, `STUDENT_ID`, `LASTNAME`, `FIRSTNAME`, `COMMENT_STATUS`, `APPLICATION_FORM`, `COLLEGE`, `STATUS`) VALUES
-(1, '20180420001-in', 'Ortega', 'Dave Peter Lorenzo', 1, 'inbound', '', ''),
-(2, '20180420001-in', 'Ortega', 'Dave Peter Lorenzo', 1, '', 'College of Education', 'Qualified'),
-(3, '20180420001-in', 'Ortega', 'Dave Peter Lorenzo', 1, '', 'Faculty of Engineering', 'Completed'),
-(4, '20180420001-in', 'Milabo', 'Aimee Maricris', 1, '', 'Faculty of Pharmacy', 'Rejected'),
-(5, '20180422002-in', 'Kim', 'Jennie', 1, 'inbound', '', ''),
-(6, '20180420001-in', 'Ortega', 'Dave Peter Lorenzo', 1, '', 'Institute of Information and Computing Sciences', 'Qualified');
+INSERT INTO `notification` (`STUDENT_COUNT`, `STUDENT_ID`, `LASTNAME`, `FIRSTNAME`, `COMMENT_STATUS`, `APPLICATION_FORM`, `COLLEGE`, `COURSE`, `STATUS`) VALUES
+(1, '20180423001-in', 'Ortega', 'Dave', 1, 'inbound', '', '', ''),
+(2, '20180423001-in', 'Ortega', 'Dave', 1, '', 'Institute of Information and Computing Sciences', 'Arki101', 'Qualified'),
+(3, '20180423001-in', 'Ortega', 'Dave', 1, '', 'Institute of Information and Computing Sciences', 'ICS101', 'Qualified'),
+(4, '20180423001-in', 'Ortega', 'Dave', 1, '', 'Institute of Information and Computing Sciences', 'IT202', 'Qualified'),
+(5, '20180423001-in', 'Ortega', 'Dave', 1, '', 'Institute of Information and Computing Sciences', 'Arki101', 'Qualified'),
+(6, '20180423001-in', 'Ortega', 'Dave', 1, '', 'Institute of Information and Computing Sciences', 'ICS101', 'Qualified'),
+(7, '20180423001-in', 'Ortega', 'Dave', 1, '', 'Institute of Information and Computing Sciences', 'IT202', 'Qualified'),
+(8, '20180423001-in', 'Ortega', 'Dave', 1, '', 'Faculty of Arts and Letters', 'IT211', 'Qualified');
 
 --
 -- Indexes for dumped tables
@@ -69,7 +72,7 @@ ALTER TABLE `notification`
 -- AUTO_INCREMENT for table `notification`
 --
 ALTER TABLE `notification`
-  MODIFY `STUDENT_COUNT` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `STUDENT_COUNT` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
