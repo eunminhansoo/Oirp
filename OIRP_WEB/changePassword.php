8bb47ae1c4e66a7efe3c02ef50b1d3a42d672e9e
Jeanina <2014072568@ust-ics.mygbiz.com>
2018-05-06 01:17:06 +0800

    change password UI

---
+++ OIRP_WEB/changePassword.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width = device-width, initial-scale = 1">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap-theme.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/style.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/custom.css">
+        <link rel="icon" href="img/ust.png" type="image/png" sizes="196x196">
+	</head>
+	<body>
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+		<script src="bootstrap-3.3.7-dist/js/jquery-3.3.1.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+		<div class="">
+		<style>
+		.danger {
+    	background-color: #ffffcc;
+    	border-left: 6px solid #ffeb3b;
+		}
+		</style>
+		<div class="header">
+			<a href="index.php"><img src='img/logo.png' height=auto class="img-responsive"></a>
+		</div>
+				
+	<div style="height: 10px; background-color: #FDC601;     box-shadow: 2px 2px 4px #888888;">
+		<div class="container-fluid">
+			<div class="navbar-header">				
+			</div>
+		</div>
+	</div>
+	<div class="container div_center_signUp"> 
+		<div class="form-group row">
+			<div class="col-sm-3"></div>
+			<div class="col-sm-6">
+				<form method="post">
+					<div class="form-group row">
+						<h3><strong>Change Password</strong></h3>
+					</div>
+					<div class="form-group row">
+						<label>Old Password</label>
+						<input type="password" name="oldpw" id="oldpw" class="form-control" required/>
+					</div>
+					<div class="form-group row">
+						<label>New Password</label>
+						<input type="password" name="newpw" id="newpw" class="form-control" required/>
+					</div>
+					<div class="form-group row">
+						<label>Confirm New Password</label>
+						<input type="password" name="confirmpw" id="confirmpw" class="form-control" required/>
+					</div>
+					<div class="form-group row">
+						<input type="submit" value="Submit" class="btn btn btn-info btn-block break">
+					</div>
+				</form>
+			</div>
+			<div class="col-sm-3"></div>
+		</div>
+	</div>
+</body>
+</html>
+			
+	
+	
--- OIRP_WEB/register.php
+++ OIRP_WEB/register.php
@@ -33,10 +33,8 @@
 		<div class="header">
 			<a href="index.php"><img src='img/logo.png' height=auto class="img-responsive"></a>
 		</div>
-		</head>
 		
-	<div style="height: 10px; background-color: #FDC601;     box-shadow: 2px 2px 4px #888888;
-">
+	<div style="height: 10px; background-color: #FDC601;     box-shadow: 2px 2px 4px #888888;">
 		<div class="container-fluid">
 			<div class="navbar-header">				
 			</div>
